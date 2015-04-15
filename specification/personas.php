c2e7cacee6b1209c29fffa11554d50334c13eee6
Ella Haines <ellatrue@yahoo.com>
2015-04-15 16:36:20 -0600

    Imported from old github repository

---
+++ specification/personas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h1>
+			Personas for Dice.com Job Description
+		</h1>
+		<h2> Job Seeker </h2>
+		<p>
+			Age: 23-60 <br/>
+			Goals: To find a technical job that matches his/her skills and submit a resume and cover letter.<br/>
+			Device: Desktop Computer <br/>
+			Platform: Windows or Mac OS <br/>
+		</p>
+		<h2> Employer</h2>
+		<p>
+			Age: 35-60 <br/>
+			Goals: To post a job description and collect resumes and cover letters from prospective employees
+				for a technology job.<br/>
+			Device: Desktop Computer <br/>
+			Platform: Windows or Mac OS <br/>
+		</p>
+
+	</body>
+</html>
---
+++ specification/usecase.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<h1>Use Cases for Dice.com Job Description</h1>
+
+		<h2> Job Seekers</h2>
+		<h3> When and Where:</h3>
+			<ul>
+				<li>Evening</li>
+				<li>From home</li>
+				<li>Consistent data and power on desktop/laptop</li>
+				<li>Possible printer</li>
+			</ul>
+		<h3> Goal:</h3>
+			<p>To apply for a job by submitting an application.</p>
+		<h3> Steps:</h3>
+			<ul>
+				<li>Read job description and determine if the job is a good fit</li>
+				<li> If job is not a good fit, consider "Similar Positions" links in right panel</li>
+				<li>Possibly save or email job for later application</li>
+				<li>Hit "apply now" button to be redirected to employer's site and begin application process</li>
+			</ul>
+
+		<h2>Employers</h2>
+		<h3> When and Where:</h3>
+			<ul>
+				<li>During work day (8am-5pm)</li>
+				<li>From office</li>
+				<li>Consistent data and power on desktop/laptop</li>
+				<li>printer available</li>
+			</ul>
+		<h3> Goal:</h3>
+			<p>Have prospective employees sent from dice page to company's job application form.</p>
+		<h3> Steps:</h3>
+			<ul>
+				<li>Post job description and requirements on Dice</li>
+				<li>Check that job description page looks acceptable</li>
+				<li>Wait, hope for applications</li>
+			</ul>
+	</body>
+</html>
