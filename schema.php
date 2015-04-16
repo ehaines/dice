22affb6bc852b7d4ebf022d3c83b999f4e19d83b
Ella Haines <ellatrue@yahoo.com>
2015-04-16 09:55:29 -0600

    Moved image into <figure> tags, added caption and CSS style for the caption.

--- schema.php
+++ schema.php
@@ -3,12 +3,18 @@
 	<head>
 		<title>Dice.com Job Listing Schema</title>
 		<style>
-			body {background-color: cornflowerblue;}
-			body {color: white; font-family: sans-serif}
+			body {color: white;
+				background-color: cornflowerblue;
+				font-family: sans-serif;}
+			body figcaption {font-weight: bold;
+				font-style: italic}
 		</style>
 	</head>
 	<body>
 		<h1>Dice.com Job Listing Schema</h1>
-		<img src="img/Dice_ERD.svg" />
+		<figure>
+			<img src="img/Dice_ERD.svg" />
+			<figcaption>An Entity-Relationship Diagram for a single Dice.com Job Listing Page</figcaption>
+		</figure>
 	</body>
 </html> 
