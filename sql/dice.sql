e5ae0f34afdbadec5d992f03b7cdfbba65a0eedb
Ella Haines <ellatrue@yahoo.com>
2015-04-16 12:05:07 -0600

    Added employer and job description tables

--- sql/dice.sql
+++ sql/dice.sql
@@ -1,4 +1,23 @@
 DROP TABLE IF EXISTS employer;
-DROP TABLE IF EXISTS location;
-DROP TABLE IF EXISTS tag;
 DROP TABLE IF EXISTS jobDescription;
+DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS jobLocation;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS jobTag;
+
+CREATE TABLE employer (
+	diceID VARCHAR(12),
+	logo TEXT,
+	website TEXT,
+	PRIMARY KEY (diceID)
+);
+
+CREATE TABLE jobDescription (
+	positionID INT UNSIGNED NOT NULL,
+	travelRequired BOOLEAN,
+	salary INT UNSIGNED,
+	dateTimePosted DATETIME,
+	text TEXT,
+	hours VARCHAR(12)
+);
+
