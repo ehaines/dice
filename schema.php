772d41e629fb9c1a4f9568b843f24718d49aec07
Ella Haines <ellatrue@yahoo.com>
2015-04-16 09:35:55 -0600

    Added Dice_ERD svg image to schema and img directory.

---
+++ schema.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Dice.com Job Listing Schema</title>
+		<style>
+			body {background-color: black;}
+			body {color: white;}
+		</style>
+	</head>
+	<body>
+		<img src="img/Dice_ERD.svg" />
+	</body>
+	
+</html> 
