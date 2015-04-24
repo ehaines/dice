dbed63afb51c4c71c6978fe20eb218677ddea227
Ella Haines <ellatrue@yahoo.com>
2015-04-24 09:37:29 -0600

    required employer class in employer-test and added some comments

--- php/classes/employer-test.php
+++ php/classes/employer-test.php
@@ -1,5 +1,9 @@
 <?php
+//require the AES 256 functions
 require_once("/etc/apache2/data-design/encrypted-config.php");
+
+//bring in/require the class being tested (Employer)
+require_once("employer.php");
 
 $config = readConfig("/etc/apache2/data-design/ehaines.ini");
 
